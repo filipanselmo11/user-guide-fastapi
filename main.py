7e335ba324b6456f64a6e666b9bc7f2d43bc2167
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-05-14 16:19:39 -0400

    feat: query params

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 from fastapi import FastAPI
-from enum import Enum
 
 app = FastAPI()
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit : int = 10):
+    return fake_items_db[skip: skip + limit]
 
 @app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
+async def read_items(item_id: str, q: str | None = None):
+    if (q):
+        return {"item_id":item_id, "q":q}
+    return {"item_id":item_id}
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+@app.get("/items/{item_id_2}")
+async def read_item(item_id_2: str, q: str | None = None, short: bool = False):
+    item = {"item_id_2":item_id_2}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update(
+            {"description": "this is an amazing item that has a long description"}
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+        )
+    return item
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if (model_name is ModelName.alexnet):
-        return {
-            "model_name": model_name,
-            "message": "Deep Learning FTW"
-        }
-    if (model_name.value == "lenet"):
-        return {
-            "model_name": model_name,
-            "message": "LeCNN all the images"
-        }
-    return {
-        "model_name": model_name,
-        "message": "Have some residuals"
-    }
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update(
+            {"description": "this is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/{item_id_3}")
+async def read_user_item(item_id_3: str, needy: str):
+    item = {"item_id":item_id_3, "needy": needy}
+    return item
