007b7f660d512c6126052abd96f32ce23049388f
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-05-15 10:01:03 -0400

    feat: request-body

--- main.py
+++ main.py
@@ -1,51 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"}
-]
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax":price_with_tax})
+    return item_dict
 
-@app.get("/items/")
-async def read_items(skip: int = 0, limit : int = 10):
-    return fake_items_db[skip: skip + limit]
-
-@app.get("/items/{item_id}")
-async def read_items(item_id: str, q: str | None = None):
-    if (q):
-        return {"item_id":item_id, "q":q}
-    return {"item_id":item_id}
-
-@app.get("/items/{item_id_2}")
-async def read_item(item_id_2: str, q: str | None = None, short: bool = False):
-    item = {"item_id_2":item_id_2}
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id":item_id, **item.model_dump()}
     if q:
-        item.update({"q":q})
-    if not short:
-        item.update(
-            {"description": "this is an amazing item that has a long description"}
-
-        )
-    return item
-
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q":q})
-    if not short:
-        item.update(
-            {"description": "this is an amazing item that has a long description"}
-        )
-    return item
-
-
-@app.get("/items/{item_id_3}")
-async def read_user_item(item_id_3: str, needy: str):
-    item = {"item_id":item_id_3, "needy": needy}
-    return item
+        result.update({"q":q})
+    return result
